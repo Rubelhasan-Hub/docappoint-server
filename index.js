23e5f59546313446997ce3dc6fc71195aa706050
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-05-18 11:47:07 +0600

    connect to mongodb and accecss all doctors data with get method

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
+require("dotenv").config();
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGO_URI
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db("docappoinment")
+        const doctorsCollection= db.collection("doctors")
+
+        app.get('/doctors', async (req, res) => {
+            const result = await doctorsCollection.find().toArray()
+            res.json(result)
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+       
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
