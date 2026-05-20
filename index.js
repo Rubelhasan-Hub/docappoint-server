6dd0d708031d7c54fe1db34a1174dc2c913d1804
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-05-21 02:33:06 +0600

    added a post app to collect the booking data and save in database

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 
 const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const cors = require('cors')
 require("dotenv").config();
+const app = express()
+
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.MONGO_URI
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,17 +30,29 @@ async function run() {
         await client.connect();
 
         const db = client.db("docappoinment")
-        const doctorsCollection= db.collection("doctors")
+        const doctorsCollection = db.collection("doctors")
+        const bookingCollection = db.collection("booking")
 
         app.get('/doctors', async (req, res) => {
-            const result = await doctorsCollection.find().toArray()
+            const result = await doctorsCollection.find().sort({ rating: -1 }).toArray()
             res.json(result)
+        })
+        app.get('/doctors/:id', async (req, res) => {
+            const { id } = req.params
+            const result = await doctorsCollection.findOne({ _id: new ObjectId(id) })
+            res.json(result)
+        })
+
+        app.post('/booking', async (req, res) => {
+            const bookingData = req.body;
+            const result = await bookingCollection.insertOne(bookingData)
+            res.json(result);
         })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-       
+
     }
 }
 run().catch(console.dir);
