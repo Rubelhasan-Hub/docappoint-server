b28187bedbcf1381641cccd9a9e4de22e37c44b8
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-05-22 00:01:06 +0600

    Slove the deploy related problem

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ const app = express()
 
 const port = process.env.PORT || 5000
 
-app.use(cors())
+app.use(
+    cors({
+        origin: "https://docappoint-client.vercel.app",
+        credentials: true,
+    })
+);
+app.options("*", cors({
+    origin: "https://docappoint-client.vercel.app",
+    credentials: true,
+}));
 app.use(express.json())
 
 
